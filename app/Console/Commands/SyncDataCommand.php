ce284371e87775e07380cd3254aea1267fb4a78c
hxvdofhxll878 <trololik882002@gmail.com>
2026-06-17 06:09:10 +0500

    Add SyncDataCommand

---
+++ app/Console/Commands/SyncDataCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+use App\Services\Importers\IncomeImporter;
+use App\Services\Importers\OrderImporter;
+use App\Services\Importers\SaleImporter;
+use App\Services\Importers\StockImporter;
+
+#[Signature('app:sync-data')]
+#[Description('Import all data from API')]
+class SyncDataCommand extends Command
+{
+    public function handle(
+        IncomeImporter $incomeImporter,
+        OrderImporter $orderImporter,
+        SaleImporter $saleImporter,
+        StockImporter $stockImporter,
+    ): int {
+        $this->info('Start importing incomes:');
+        $incomeImporter->import('2020-01-01', '2026-12-31');
+
+
+        $this->info('Start importing orders:');
+        $orderImporter->import('2020-01-01', '2026-12-31');
+
+
+        $this->info('Start importing sales:');
+        $saleImporter->import('2020-01-01', '2026-12-31');
+
+
+        $this->info('Start importing stocks:');
+        $stockImporter->import(now()->subDay()->format('Y-m-d'));
+
+        $this->info('Finished importing data');
+
+        return self::SUCCESS;
+    }
+}
